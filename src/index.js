aa33dcc73ab9b9d9a5aa981852d7c569c0ebb9a8
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-03-10 21:34:35 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -231,11 +231,10 @@ app.delete("/api/delete/:id", validateToken, (req, res) => {
 const PORT = process.env.PORT || 1337;
 
 if (process.env.NODE_ENV === "production") {
-  console.log(__dirname);
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "./frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "./frontend/build/index.html"));
+    res.sendFile(path.join(__dirname + "frontend/build/index.html"));
   });
 }
 
