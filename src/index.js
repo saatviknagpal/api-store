0d0e4a89f3b4efe586378bd000eb47a7f38acf61
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-03-10 21:55:02 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -234,7 +234,7 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+    res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
   });
 }
 
